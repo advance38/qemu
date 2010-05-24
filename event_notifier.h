410e7868921185e80fb07b0792485f4b7ef4b991
Paolo Bonzini <pbonzini@redhat.com>
2010-05-24 17:11:44 +0200

    event_notifier: add event_notifier_set_handler
    Win32 event notifiers are not file descriptors, so they cannot use
    qemu_set_fd_handler.  Add a specific function that wraps the call,
    and make it pass the EventNotifier as the opaque value so that it
    will be used with container_of.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- event_notifier.h
+++ event_notifier.h
@@ -19,11 +19,14 @@ struct EventNotifier {
         int fd;
 };
 
+typedef void EventNotifierHandler(EventNotifier *);
+
 int event_notifier_init(EventNotifier *, int active);
 void event_notifier_cleanup(EventNotifier *);
 int event_notifier_get_fd(EventNotifier *);
 int event_notifier_set(EventNotifier *);
 int event_notifier_test_and_clear(EventNotifier *);
+int event_notifier_set_handler(EventNotifier *, EventNotifierHandler *);
 
 void event_notifier_init_fd(EventNotifier *, int);
 
