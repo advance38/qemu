b5c32f7a549844b491d6c20c28fd07cb794ca7d0
Sripathi Kodi <sripathik@in.ibm.com>
2010-10-18 15:22:30 +0200

    Add read-write lock to QEMU
    Signed-off-by: Sripathi Kodi <sripathik@in.ibm.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- qemu-thread.h
+++ qemu-thread.h
@@ -4,6 +4,7 @@
 #include <inttypes.h>
 
 typedef struct QemuMutex QemuMutex;
+typedef struct QemuRWMutex QemuRWMutex;
 typedef struct QemuCond QemuCond;
 typedef struct QemuThread QemuThread;
 
@@ -18,6 +19,11 @@ void qemu_mutex_destroy(QemuMutex *mutex);
 void qemu_mutex_lock(QemuMutex *mutex);
 int qemu_mutex_trylock(QemuMutex *mutex);
 void qemu_mutex_unlock(QemuMutex *mutex);
+
+void qemu_rwmutex_init(QemuRWMutex *mutex);
+void qemu_rwmutex_rdlock(QemuRWMutex *mutex);
+void qemu_rwmutex_wrlock(QemuRWMutex *mutex);
+void qemu_rwmutex_unlock(QemuRWMutex *mutex);
 
 void qemu_cond_init(QemuCond *cond, QemuMutex *mutex);
 void qemu_cond_destroy(QemuCond *cond);
